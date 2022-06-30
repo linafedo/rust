fa56acd5b629e1b10637d160ec1a6bb93fb33a87
linafedo <gv.fedo@gmail.com>
2022-06-30 13:05:02 +0300

    split code into files

---
+++ src/guess_task.rs
@@ -0,0 +1,39 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+pub fn guess_task() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let attempt_count = 3;
+    let mut step = 1;
+    while step <= attempt_count {
+        if step <= attempt_count {
+            println!("Please, inter your guess");
+            let mut guess = String::new();
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(result) => result,
+                Err(_) => continue
+            };
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => {
+                    println!("Too small!");
+                }
+                Ordering::Equal => {
+                    println!("You win!");
+                    return;
+                }
+                Ordering::Greater => {
+                    println!("Too big!")
+                }
+            }
+        }
+        step += 1;
+    }
+    println!("YOU LOST :(")
+}
